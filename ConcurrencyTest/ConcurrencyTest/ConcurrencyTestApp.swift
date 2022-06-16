3ec8ff474f8e3ad9d182348c3a86ec6ec585735a
ELO :) <eliasjan@ciandt.com>
2022-06-15 21:09:51 -0300

    add exemplo que trava a ui ao realizar calculo

---
+++ ConcurrencyTest/ConcurrencyTest/ConcurrencyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyTestApp.swift
+//  ConcurrencyTest
+//
+//  Created by cit on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencyTest/ConcurrencyTest/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  ConcurrencyTest
+//
+//  Created by cit on 15/06/22.
+//
+// 1 ao apertar  o botao -  a ui vai ficar travada calculando la os numeros 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            DatePicker(selection: .constant(Date()), label: { Text("Date")})
+                .labelsHidden()
+            Button(action: calculatePrime, label: { Text("Calculate Primes")})
+            Spacer()
+        }
+    }
+}
+
+func calculatePrime(){
+    for number in 0...1_000_000 {
+        let isPrimeNumber = isPrime(number: number)
+        print("\(number) is prime: \(isPrimeNumber)")
+    }
+}
+
+func isPrime(number: Int)-> Bool{
+    if number <= 1 {
+        return false
+    }
+    if number <= 3 {
+        return false
+    }
+    var i = 2
+    
+    while i * i <= number {
+        if number % i == 0{
+            return false
+        }
+        i = i + 2
+    }
+    return true
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
