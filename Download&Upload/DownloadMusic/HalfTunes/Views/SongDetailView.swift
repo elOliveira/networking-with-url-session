18e58d9855fd547a495871c7eb1f0bea74c696f5
ELO :) <eliasjan@ciandt.com>
2022-06-16 17:48:32 -0300

    swift ui  add download image + att

--- Download&Upload/DownloadMusic/HalfTunes/Views/SongDetailView.swift
+++ Download&Upload/DownloadMusic/HalfTunes/Views/SongDetailView.swift
@@ -3,16 +3,15 @@
 struct SongDetailView: View {
 
   @Binding var musicItem: MusicItem
-  @State private var playMusic = false
   @ObservedObject var download = SongDownload()
-  
-  var musicImage: UIImage? = nil
+  @State private var playMusic = false
+  @State private var musicImage = UIImage(named: "c_urlsession_card_artwork")!
   
   var body: some View {
     VStack {
       GeometryReader { reader in
         VStack {
-          Image("c_urlsession_card_artwork")
+            Image(uiImage: self.musicImage)
             .resizable()
             .aspectRatio(contentMode: .fit)
             .frame(height: reader.size.height / 2)
@@ -23,15 +22,30 @@ struct SongDetailView: View {
           Text(self.musicItem.collectionName)
             Button(action: self.botaoDownloadClick){
             Text(self.download.downloadLocation == nil ? "Baixar" : "Escutar")
-          }
-        }
-      }
-    }
-        .sheet(isPresented: self.$playMusic) {
-            AudioPlayer(songUrl: self.download.downloadLocation!)
-        }
+          }.sheet(isPresented: self.$playMusic) {
+              AudioPlayer(songUrl: self.download.downloadLocation!)
+          }.onAppear(perform: displayAlbumArt)
+        }.onAppear(perform: displayAlbumArt)
+      }.onAppear(perform: displayAlbumArt)
+    }.onAppear(perform: displayAlbumArt)
   }
+    
+    func displayAlbumArt(){
+        guard let albumImageUrl = URL(string: musicItem.artwork) else { return }
+        
+        let task = URLSession.shared.downloadTask(with: albumImageUrl) { location, response, error in
 
+            guard let location = location,
+                  let imageData = try? Data(contentsOf: location),
+              let image = UIImage(data: imageData) else { return }
+            
+            DispatchQueue.main.async {
+                self.musicImage = image
+            }
+        }
+        task.resume()
+    }
+    
     func botaoDownloadClick(){
         if self.download.downloadLocation == nil{
             guard let previewUrl = self.musicItem.previewUrl else { return }
@@ -40,13 +54,10 @@ struct SongDetailView: View {
             self.playMusic = true
         }
     }
-
 }
 
 
 struct SongDetailView_Previews: PreviewProvider {
-
-  
   struct PreviewWrapper: View {
     @State private var musicItem = MusicItem(id: 192678693, artistName: "Leonard Cohen", trackName: "Hallelujah", collectionName: "The Essential Leonard Cohen", preview: "https://audio-ssl.itunes.apple.com/itunes-assets/Music/16/10/b2/mzm.muynlhgk.aac.p.m4a", artwork: "https://is1-ssl.mzstatic.com/image/thumb/Music/v4/77/17/ab/7717ab31-46f9-48ca-7250-9f565306faa7/source/1000x1000bb.jpg")
     
@@ -59,4 +70,3 @@ struct SongDetailView_Previews: PreviewProvider {
     PreviewWrapper()
   }
 }
-
